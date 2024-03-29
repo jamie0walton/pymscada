bd4c1aa560f4d1384677f7dbdcd1bc48b9d44ba1
Jamie Walton <jamie@walton.net.nz>
2024-03-30 10:35:21 +1300

    repackaged modules in main.py

--- src/pymscada/main.py
+++ src/pymscada/main.py
@@ -19,101 +19,220 @@ from pymscada.www_server import WwwServer
 from pymscada.validate import validate
 
 
-MODULES = {}
+class Module():
+    """Default Module."""
+
+    name = None
+    help = None
+    epilog = None
+    module = None
+    config = True
+    tags = True
+    sub = None
+
+    def __init__(self, subparser: argparse._SubParsersAction):
+        """Add arguments common to all subparsers."""
+        self.sub = subparser.add_parser(
+            self.name, help=self.help, epilog=self.epilog)
+        self.sub.set_defaults(app=self)
+        if self.config:
+            self.sub.add_argument(
+                '--config', metavar='file', default=f'{self.name}.yaml',
+                help=f"Config file, default is '{self.name}.yaml'")
+        if self.tags:
+            self.sub.add_argument(
+                '--tags', metavar='file', default='tags.yaml',
+                help="Tags file, default is 'tags.yaml'")
+        self.sub.add_argument('--verbose', action='store_true',
+                              help="Set level to logging.INFO")
 
 
-async def bus(options):
-    """Return bus module."""
-    config = Config(options.config)
-    return BusServer(**config)
+class _Bus(Module):
+    """Bus Server."""
+
+    name = 'bus'
+    help = 'run the message bus'
+    tags = False
+
+    def run_once(self, options):
+        """Create the module."""
+        config = Config(options.config)
+        self.module = BusServer(**config)
 
 
-async def wwwserver(options):
-    """Return wwwserver module."""
-    config = Config(options.config)
-    tag_info = dict(Config(options.tags))
-    return WwwServer(tag_info=tag_info, **config)
+class _WwwServer(Module):
+    """WWW Server Module."""
+
+    name = 'wwwserver'
+    help = 'serve web pages'
+
+    def run_once(self, options):
+        """Create the module."""
+        config = Config(options.config)
+        tag_info = dict(Config(options.tags))
+        self.module = WwwServer(tag_info=tag_info, **config)
 
 
-async def history(options):
-    """Return history module."""
-    config = Config(options.config)
-    tag_info = dict(Config(options.tags))
-    return History(tag_info=tag_info, **config)
+class _History(Module):
+    """History Module."""
+
+    name = 'history'
+    help = 'collect and serve history'
+
+    def run_once(self, options):
+        """Create the module."""
+        config = Config(options.config)
+        tag_info = dict(Config(options.tags))
+        self.module = History(tag_info=tag_info, **config)
 
 
-async def files(options):
-    """TODO Return files module."""
-    config = Config(options.config)
-    return Files(**config)
+class _Files(Module):
+    """Bus Module."""
+
+    name = 'files'
+    help = 'receive and send files'
+    tags = False
+
+    def run_once(self, options):
+        """Create the module."""
+        config = Config(options.config)
+        self.module = Files(**config)
 
 
-async def console(_options):
-    """TODO Return console module."""
-    return Console()
+class _Console(Module):
+    """Bus Module."""
+
+    name = 'console'
+    help = 'interactive bus console'
+    config = False
+    tags = False
+
+    def run_once(self, _options):
+        """Create the module."""
+        self.module = Console()
 
 
-async def _checkout(options):
-    """Checkout files in current working directory."""
-    checkout(overwrite=options.overwrite, diff=options.diff)
-    return
+class _checkout(Module):
+    """Bus Module."""
+
+    name = 'checkout'
+    help = 'create example config files'
+    epilog = """
+        To add to systemd `f="pymscada-bus" && cp config/$f.service
+        /lib/systemd/system && systemctl enable $f && systemctl start
+        $f`"""
+    config = False
+    tags = False
+
+    def __init__(self, subparser: argparse._SubParsersAction):
+        super().__init__(subparser)
+        self.sub.add_argument(
+            '--overwrite', action='store_true', default=False,
+            help='checkout may overwrite files, CARE!')
+        self.sub.add_argument(
+            '--diff', action='store_true', default=False,
+            help='compare default with existing')
+
+    def run_once(self, options):
+        """Create the module."""
+        checkout(overwrite=options.overwrite, diff=options.diff)
 
 
-async def _validate(options):
-    """Validate config files."""
-    r, e, p = validate(options.path)
-    if r:
-        print(f'Config files in {p} valid.')
-    else:
-        print(e)
-    return
+class _validate(Module):
+    """Bus Module."""
+
+    name = 'validate'
+    help = 'validate config files'
+    config = False
+    tags = False
+
+    def __init__(self, subparser: argparse._SubParsersAction):
+        super().__init__(subparser)
+        self.sub.add_argument(
+            '--path', metavar='file',
+            help='default is current working directory')
+
+    def run_once(self, options):
+        """Create the module."""
+        r, e, p = validate(options.path)
+        if r:
+            print(f'Config files in {p} valid.')
+        else:
+            print(e)
 
 
-async def modbusserver(options):
-    """Return modbusserver module."""
-    config = Config(options.config)
-    return ModbusServer(**config)
+class _ModbusServer(Module):
+    """Bus Module."""
+
+    name = 'modbusserver'
+    help = 'receive modbus messages'
+    epilog = """
+        Needs `setcap CAP_NET_BIND_SERVICE=+eip /usr/bin/python3.nn` to
+        bind to port 502."""
+    tags = False
+
+    def run_once(self, options):
+        """Create the module."""
+        config = Config(options.config)
+        self.module = ModbusServer(**config)
 
 
-async def modbusclient(options):
-    """Return modbusclient module."""
-    config = Config(options.config)
-    return ModbusClient(**config)
+class _ModbusClient(Module):
+    """Bus Module."""
+
+    name = 'modbusclient'
+    help = 'poll/write to modbus devices'
+    tags = False
+
+    def run_once(self, options):
+        """Create the module."""
+        config = Config(options.config)
+        self.module = ModbusClient(**config)
 
 
-async def logixclient(options):
-    """Return logixclient module."""
-    config = Config(options.config)
-    return LogixClient(**config)
+class _LogixClient(Module):
+    """Bus Module."""
+
+    name = 'logixclient'
+    help = 'poll/write to logix devices'
+    tags = False
+
+    def run_once(self, options):
+        """Create the module."""
+        config = Config(options.config)
+        self.module = LogixClient(**config)
 
 
-async def ping(options):
-    """Return logixclient module."""
-    if sys.platform.startswith("win"):
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    config = Config(options.config)
-    return PingClient(**config)
+class _PingClient(Module):
+    """Bus Module."""
+
+    name = 'ping'
+    help = 'ping a list of addresses, return time'
+    epilog = """
+        Needs `setcap CAP_NET_RAW+ep /usr/bin/python3.nn` to open SOCK_RAW
+    """
+    tags = False
+
+    def run_once(self, options):
+        """Create the module."""
+        if sys.platform.startswith("win"):
+            asyncio.set_event_loop_policy(
+                asyncio.WindowsSelectorEventLoopPolicy())
+        config = Config(options.config)
+        self.module = PingClient(**config)
 
 
-async def snmpclient(options):
-    """Return snmpclient module."""
-    config = Config(options.config)
-    return SnmpClient(**config)
+class _SnmpClient(Module):
+    """Bus Module."""
 
+    name = 'snmpclient'
+    help = 'poll snmp oids'
+    tags = False
 
-def add_subparser_defaults(
-        parser: argparse._SubParsersAction,
-        name: str, call, help: str, epilog: str):
-    """Add arguments common to all subparsers."""
-    s = parser.add_parser(name, help=help, epilog=epilog)
-    s.set_defaults(get_module=call, module=name)
-    s.add_argument('--config', metavar='file', default=None,
-                   help=f"Config file, default is '{name}.yaml'")
-    s.add_argument('--tags', metavar='file', default=None,
-                   help="Tags file, default is 'tags.yaml'")
-    s.add_argument('--verbose', action='store_true',
-                   help="Set level to logging.INFO")
-    return s
+    def run_once(self, options):
+        """Create the module."""
+        config = Config(options.config)
+        self.module = SnmpClient(**config)
 
 
 def args(_version: str):
@@ -123,41 +242,19 @@ def args(_version: str):
         description='Connect IO, logic, applications, and webpage UI',
         epilog=f'Python Mobile SCADA {_version}'
     )
-    subparsers = parser.add_subparsers(title='module')
-    for module, func, help, epilog in [
-        ['bus', bus, 'run the message bus', None],
-        ['wwwserver', wwwserver, 'serve web pages', None],
-        ['history', history, 'collect and serve history', None],
-        ['files', files, 'receive and send files', None],
-        ['console', console, 'interactive bus console', None],
-        ['checkout', _checkout, 'create example config files', """
-            To add to systemd `f="pymscada-bus" && cp config/$f.service
-            /lib/systemd/system && systemctl enable $f && systemctl start
-            $f`"""],
-        ['validate', _validate, 'validate config files', None],
-        ['modbusserver', modbusserver, 'receive modbus messages', """
-            Needs `setcap CAP_NET_BIND_SERVICE=+eip /usr/bin/python3.nn` to
-            bind to port 502"""],
-        ['modbusclient', modbusclient, 'poll/write to modbus devices', None],
-        ['ping', ping, 'ping a list of addresses, return time', """
-            Needs `setcap CAP_NET_RAW+ep /usr/bin/python3.nn` to open SOCK_RAW
-            """],
-        ['logixclient', logixclient, 'poll/write to logix devices', None],
-        ['snmpclient', snmpclient, 'poll snmp oids', None],
-    ]:
-        modparser = add_subparser_defaults(subparsers, module, func, help,
-                                           epilog)
-        if module == 'checkout':
-            modparser.add_argument(
-                '--overwrite', action='store_true', default=False,
-                help='checkout may overwrite files, CARE!')
-            modparser.add_argument(
-                '--diff', action='store_true', default=False,
-                help='compare default with existing')
-        elif module == 'validate':
-            modparser.add_argument(
-                '--path', metavar='file',
-                help='default is current working directory')
+    s = parser.add_subparsers(title='module')
+    _Bus(s)
+    _WwwServer(s)
+    _History(s)
+    _Files(s)
+    _Console(s)
+    _checkout(s)
+    _validate(s)
+    _ModbusServer(s)
+    _ModbusClient(s)
+    _LogixClient(s)
+    _SnmpClient(s)
+    _PingClient(s)
     return parser.parse_args()
 
 
@@ -168,12 +265,7 @@ async def run():
     options = args(_version)
     if options.verbose:
         logging.basicConfig(level=logging.INFO)
-    if options.config is None:
-        options.config = f'{options.module}.yaml'
-    if options.tags is None:
-        options.tags = 'tags.yaml'
-    module = await options.get_module(options)
-    if module is None:
-        return
-    await module.start()
-    await asyncio.get_event_loop().create_future()
+    options.app.run_once(options)
+    if options.app.module is not None:
+        await options.app.module.start()
+        await asyncio.get_event_loop().create_future()
